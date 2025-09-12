234dc2d09ea0ac159439c29e7ff0cb044d441f83
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 18:44:27 -0500

    create signIn function

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def singin(request):
-    return render(request, 'singin.html')
+def signIn(request):
+    if request.method == 'GET':
+        return render(request, 'signIn.html')
+    elif request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signIn.html', {
+                'error': "Usuario o contraseña equivocados",
+            })
+        login(request, user)
+        return redirect("feed")
+        
 
 
 def singup(request):
