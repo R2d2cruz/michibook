d82124cf719f39d0e9457c77fa42cc9819ac0100
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 15:55:35 -0500

    create Maullido model

--- apps/maullidos/models.py
+++ apps/maullidos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Maullido(models.Model):
+    postUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="maullidos")
+    body = models.CharField(max_length=151)
+    postDate = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default = 0)
+    dislikes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.postUser + " - " + self.postDate
