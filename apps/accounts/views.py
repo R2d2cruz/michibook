2ae28358868202e19200c97603a24452e91dffef
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 18:44:43 -0500

    create signUp function

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
 
 # Create your views here.
 def signIn(request):
@@ -18,5 +19,24 @@ def signIn(request):
         
 
 
-def singup(request):
-    return render(request, 'singup.html')
+def signUp(request):
+    if request.method == 'GET':
+        return render(request, 'signUp.html', {
+            'form': UserCreationForm,
+        })
+    elif request.method == 'POST':
+        error = ""
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect("feed")
+            except IntegrityError:
+                error = "Usuario ya existe"
+        else:
+            error = "Las contraselas no son iguales"
+        return render(request, 'signUp.html', {
+            'form': UserCreationForm,
+            'error': error,
+        })
