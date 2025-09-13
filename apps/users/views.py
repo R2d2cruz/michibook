3ac3487bee309511016a7f0c998fbe65a24abedf
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 23:38:16 -0500

    create userProfile function

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 
 # Create your views here.
-def userProfile(request, user_name):
-    return render(request, 'userProfile.html')
+def userProfile(request, username):
+    try:
+        user = get_object_or_404(User, username=username)
+        return render(request, 'userProfile.html', {
+            'user': user
+        })
+    except:
+        return render(request, 'userProfile.html', {
+            'error': "Usuario " + username + " no encontrado"
+        })
 
 
 def users(request):
