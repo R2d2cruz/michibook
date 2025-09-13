12a221644d13d9e1303fe9d59c7e27e4d112821e
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 23:34:43 -0500

    remove useless prints

--- apps/maullidos/views.py
+++ apps/maullidos/views.py
@@ -6,8 +6,6 @@ def feed(request):
     # Obtiene todos los maullidos en orden descendente por fecha
     posts = Maullido.objects.all().order_by('-postDate')
     
-    for m in Maullido.objects.all():
-        print(m.id, m.postUser, m.postUser.username if m.postUser else None)
     return render(request, 'feed.html', {
         'posts': posts
     })
