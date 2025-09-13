cdb2e489cd4d33017a2b4694bf78b1578291f850
R2d2cruz <r2d2cruz@gmail.com>
2025-09-13 17:56:39 -0500

    create MaullidoAdmin model

--- apps/maullidos/admin.py
+++ apps/maullidos/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Maullido
 
+class MaullidoAdmin(admin.ModelAdmin):
+    readonly_fields = ('postDate', )
+    list_display = ('postUser', 'body', 'postDate', 'likes', 'dislikes')  # 👈 columnas visibles
+    list_filter = ('postDate', 'postUser')  # 👈 filtros a la derecha
+    search_fields = ('body', 'postUser__username')  # 👈 buscador
+    ordering = ('-postDate',)  # 👈 orden por defecto
+
 # Register your models here.
-admin.site.register(Maullido)
+admin.site.register(Maullido, MaullidoAdmin)
