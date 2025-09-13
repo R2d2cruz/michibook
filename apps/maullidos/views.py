fcaab0222937c95b927eebd044ebeef289020c9a
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 22:25:29 -0500

    create feed principal function

--- apps/maullidos/views.py
+++ apps/maullidos/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Maullido
 
 # Create your views here.
 def feed(request):
-    return render(request, 'feed.html')
+    # Obtiene todos los maullidos en orden descendente por fecha
+    posts = Maullido.objects.all().order_by('-postDate')
+    
+    for m in Maullido.objects.all():
+        print(m.id, m.postUser, m.postUser.username if m.postUser else None)
+    return render(request, 'feed.html', {
+        'posts': posts
+    })
