88fbd10e920127690f72f77d98e015df41051c5d
R2d2cruz <r2d2cruz@gmail.com>
2025-09-12 23:27:38 -0500

    change UserProile biography and birthDate

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    biography = models.TextField(blank=True)
-    birthDate = models.DateTimeField(blank=True)
+    biography = models.TextField(null=True, blank=True)
+    birthDate = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return "Perfil de usuario " + self.user.username
