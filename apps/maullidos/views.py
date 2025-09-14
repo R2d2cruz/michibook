a8f44a55838087efd8c4742b5d9b27542c8ebfa7
R2d2cruz <r2d2cruz@gmail.com>
2025-09-13 22:47:31 -0500

    create reactMaullido basic function

--- apps/maullidos/views.py
+++ apps/maullidos/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Maullido
 
 # Create your views here.
@@ -8,4 +10,21 @@ def feed(request):
     
     return render(request, 'feed.html', {
         'posts': posts
+    })
+
+
+def reactMaullido(request, maullido_id):
+    try:
+        maullido = get_object_or_404(Maullido, id=maullido_id)
+    except Maullido.DoesNotExist:
+        return JsonResponse({"error": "Maullido no encontrado"}, status=404)
+    action = request.POST.get("action")
+    if action == "like":
+        maullido.likes += 1
+    elif action == "dislike":
+        maullido.dislikes += 1
+    maullido.save()
+    return JsonResponse({
+        "likes": maullido.likes,
+        "dislikes": maullido.dislikes
     })
