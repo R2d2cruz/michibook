0a77399a70030018c5425d75d06061eba36e61f5
R2d2cruz <r2d2cruz@gmail.com>
2025-09-13 21:20:07 -0500

    add logic of create new "maullido"

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from apps.maullidos.models import Maullido
 
 
 # Create your views here.
 def userProfile(request, username):
     try:
-        user = get_object_or_404(User, username=username)
-        posts = user.maullidos.all()
-        return render(request, 'userProfile.html', {
-            'user': user,
-            'posts': posts,
-        })
+        userProfile = get_object_or_404(User, username=username)
     except User.DoesNotExist:
         return render(request, 'userProfile.html', {
             'error': "Usuario " + username + " no encontrado"
         })
+    if request.method == "POST":
+        user = request.user
+        if username == user.username:
+            maullido = Maullido.objects.create(postUser= user ,body= request.POST['maullido'])
+            maullido.save()
+            return redirect('userProfile', username)
+    posts = userProfile.maullidos.all()
+    return render(request, 'userProfile.html', {
+        'userProfile': userProfile,
+        'posts': posts,
+    })
 
 
 def users(request):
