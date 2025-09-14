2f9199e53f6bbe8a2840134e02853b7aa8e795dd
R2d2cruz <r2d2cruz@gmail.com>
2025-09-14 13:57:25 -0500

    create userEditProfile function

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from apps.maullidos.models import Maullido
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
 
 
 # Create your views here.
@@ -29,4 +31,31 @@ def users(request):
     users = User.objects.all
     return render(request, 'users.html', {
         'users': users,
+    })
+    
+
+@login_required
+def userEditProfile(request, username):
+    try:
+        user = get_object_or_404(User, username=username)
+        userProfile = get_object_or_404(UserProfile, user=user)
+        print(userProfile.birthDate)
+    except User.DoesNotExist:
+        return render(request, 'userProfile.html', {
+            'error': "Usuario " + username + " no encontrado"
+        })
+    if request.method == "POST":
+        userServer = request.user
+        if username == userServer.username:
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.email = request.POST['email']
+            user.save()
+            userProfile.biography = request.POST['biography']
+            userProfile.birthDate = request.POST['birthDate']
+            userProfile.save()
+            return redirect('userProfile', username)
+    return render(request, 'userEditProfile.html', {
+        "user": user,
+        "profile": userProfile
     })
