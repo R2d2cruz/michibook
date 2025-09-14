6edee7aad34078d8d87b68ce1d24ed3d815aa4af
R2d2cruz <r2d2cruz@gmail.com>
2025-09-14 13:58:04 -0500

    add userEditProfile to urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import userProfile, users
+from .views import userProfile, users, userEditProfile
 
 urlpatterns = [
     path('<str:username>/', userProfile, name="userProfile"),
-    path('', users, name="users")
+    path('', users, name="users"),
+    path('<str:username>/profile/', userEditProfile, name="userEditProfile"),
 ]
