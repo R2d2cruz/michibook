418f5bf1468b31e75738ba17a09320411a837dc3
R2d2cruz <r2d2cruz@gmail.com>
2025-09-13 22:48:43 -0500

    create url for reactions

--- apps/maullidos/urls.py
+++ apps/maullidos/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import feed
+from .views import feed, reactMaullido
 
 urlpatterns = [
-    path('', feed, name= 'feed')
+    path('', feed, name= 'feed'),
+    path('reactMaullido/<int:maullido_id>', reactMaullido, name="reactMaullido")
 ]
