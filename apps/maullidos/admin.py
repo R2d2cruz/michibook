5c7cd7022d336140574feabd04bedb972537c0f5
R2d2cruz <r2d2cruz@gmail.com>
2025-09-14 19:32:48 -0500

    add MaullidoReaction to admin

--- apps/maullidos/admin.py
+++ apps/maullidos/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Maullido
+from .models import Maullido, MaullidoReaction
 
 class MaullidoAdmin(admin.ModelAdmin):
     readonly_fields = ('postDate', )
-    list_display = ('postUser', 'body', 'postDate', 'likes', 'dislikes')  # 👈 columnas visibles
+    list_display = ('postUser', 'body', 'postDate')  # 👈 columnas visibles
     list_filter = ('postDate', 'postUser')  # 👈 filtros a la derecha
     search_fields = ('body', 'postUser__username')  # 👈 buscador
     ordering = ('-postDate',)  # 👈 orden por defecto
 
+class MaullidoReactionAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', 'maullido')
+    list_display = ('user', 'maullido', 'reaction')  # 👈 columnas visibles
+    list_filter = ('user', 'maullido', 'reaction')  # 👈 filtros a la derecha
+    search_fields = ('user', 'maullido', 'reaction')  # 👈 buscador
+    ordering = ('maullido',)  # 👈 orden por defecto
+
 # Register your models here.
 admin.site.register(Maullido, MaullidoAdmin)
+admin.site.register(MaullidoReaction, MaullidoReactionAdmin)
