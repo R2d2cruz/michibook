34f78e7e293a0cef1f09d8c25ffecb5f9b47da2f
R2d2cruz <r2d2cruz@gmail.com>
2025-09-14 19:45:00 -0500

    add logout url

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import signIn, signUp
+from .views import signIn, signUp, signOut
 
 urlpatterns = [
     path('login/', signIn, name="login"),
-    path('registrar/', signUp, name="register")
+    path('registrar/', signUp, name="register"),
+    path('logout/', signOut, name="logout"),
 ]
