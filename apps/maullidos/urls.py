e3e205fde76796d30f912df1920c96d94aa5f314
R2d2cruz <r2d2cruz@gmail.com>
2025-09-16 09:55:16 -0500

    remove reactMaullido function

--- apps/maullidos/urls.py
+++ apps/maullidos/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import feed, reactMaullido
+from .views import feed
 
 urlpatterns = [
     path('', feed, name= 'feed'),
-    path('reactMaullido/<int:maullido_id>', reactMaullido, name="reactMaullido")
 ]
--- apps/maullidos/views.py
+++ apps/maullidos/views.py
@@ -13,34 +13,3 @@ def feed(request):
     return render(request, 'feed.html', {
         'posts': posts
     })
-
-
-@login_required
-@require_POST
-def reactMaullido(request, maullido_id):
-    maullido = get_object_or_404(Maullido, id=maullido_id)
-    action = request.POST.get("action")
-
-    if action not in dict(MaullidoReaction.REACTION_CHOICES):
-        return JsonResponse({"error": "Reacción no válida"}, status=400)
-
-    reaction, created = MaullidoReaction.objects.get_or_create(
-        user=request.user,
-        maullido=maullido,
-        defaults={'reaction': action}
-    )
-
-    if not created:
-        if reaction.reaction == action:
-            reaction.delete()
-        else:
-            reaction.reaction = action
-            reaction.save()
-
-    return JsonResponse({
-        "happys": maullido.happys_count,
-        "sads": maullido.sads_count,
-        "loves": maullido.loves_count,
-        "shockeds": maullido.shockeds_count,
-        "angrys": maullido.angrys_count,
-    })
