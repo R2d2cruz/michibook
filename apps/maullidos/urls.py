932aeea0cc1a6208c27c408f57fc970c8ff27d28
R2d2cruz <r2d2cruz@gmail.com>
2025-09-16 12:17:17 -0500

    add createMaullido url

--- apps/maullidos/urls.py
+++ apps/maullidos/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import feed
+from .views import feed, createMaullido
 
 urlpatterns = [
     path('', feed, name= 'feed'),
+    path('maullidoCreate', createMaullido, name="userMaullidoCreate"),
 ]
