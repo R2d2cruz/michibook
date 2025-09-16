c420e130c0ac068f1901eea7cdeeb6aa2844d3eb
R2d2cruz <r2d2cruz@gmail.com>
2025-09-16 12:27:53 -0500

    add post update message in createMaullido function

--- apps/maullidos/views.py
+++ apps/maullidos/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from .models import Maullido
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 # Create your views here.
 def feed(request):
@@ -23,4 +26,18 @@ def createMaullido(request):
         
         maullido = Maullido.objects.create(postUser=request.user, body=text)
         maullido.save()
+        
+        html = render_to_string("maullido.html", {"post": maullido})
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "maullidos",
+            {
+                "type": "sendNewMaullido",
+                "html": html,
+                "username": maullido.postUser.username,
+            }
+        )
+
+        return JsonResponse({"success": True})
         
