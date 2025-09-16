32bbef7973b6a2e248b2e5a76b6ad4e545ee288f
R2d2cruz <r2d2cruz@gmail.com>
2025-09-16 09:51:55 -0500

    create webSocket in asgi.py

--- michibook/asgi.py
+++ michibook/asgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+import apps.maullidos.routing as maullidosRouting
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'michibook.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(maullidosRouting.websocket_urlpatterns)
+    ),
+})
