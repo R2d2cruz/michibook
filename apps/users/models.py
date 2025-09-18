21d143d0454ef1a3f34afdff6bd85e89415e360f
R2d2cruz <r2d2cruz@gmail.com>
2025-09-18 12:27:58 -0500

    add avatarChoice in UserProfile model

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,9 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
+    AVATAR_CHOICES = [
+        ("blackCat", "Gato Negro"),
+        ("orangeCat", "Gato Naranja"),
+        ("whiteCat", "Gato Blanco"),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     biography = models.TextField(null=True, blank=True)
     birthDate = models.DateField(null=True, blank=True)
+    avatarChoice = models.CharField(max_length=20, choices=AVATAR_CHOICES, default="blackCat")
 
     def __str__(self):
         return "Perfil de usuario " + self.user.username
